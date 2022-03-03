be5aa3556070108aa54124fe44aa7e3cf8d252e5
honganhcs <e0560140@u.nus.edu>
2022-03-03 16:46:20 +0800

    Add AssessmentResultsList, AssessmentResults and StudentResult classes to seedu.address.model.assessment package
    StudenResult represents the result of a single student for an assessment and contains an NusNetId and Score.
    AssessmentResults contains a list of StudentResults and has an AssessmentName (is associated with an Assessment).
    AssessmentResultsList contains a list of AssessmentResults and has a TutorialName (inside a Tutorial).
    
    An AssessmentResultsList is associated with a Tutorial while the UniqueAssessmentList is associated with the ModelManager or AddresBook.
    
    AssessmentResults and AssessmentResultsList both contain ObservableLists for the same reason as in the previous commit.
    
    Note: the AssessmentNotFoundException is meant for the previous commit but DuplicateStudentResultException is added in the previous commit by accident.

---
+++ src/main/java/seedu/address/model/assessment/AssessmentResults.java
@@ -0,0 +1,73 @@
+package seedu.address.model.assessment;
+
+import static java.util.Objects.requireNonNull;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.assessment.exceptions.DuplicateStudentResultException;
+
+/**
+ * Contains a list of the students' results for a particular Assessment (found in UniqueAssessmentList).
+ * The students are in the same Tutorial.
+ * Has a whole-part relationship with StudentResult.
+ *
+ * TODO: implement remove and setStudentResult (for updating student's result)
+ * @see seedu.address.model.person.UniquePersonList
+ * @see StudentResult
+ */
+public class AssessmentResults {
+    // Identity field
+    private final AssessmentName assessmentName;
+
+    // Data field
+    private final ObservableList<StudentResult> results = FXCollections.observableArrayList();
+    private final ObservableList<StudentResult> unmodifiableResults =
+            FXCollections.unmodifiableObservableList(results);
+
+    /**
+     * Constructs an AssessmentResults.
+     *
+     * @param name name of the Assessment.
+     */
+    public AssessmentResults(AssessmentName name) {
+        requireNonNull(name);
+        assessmentName = name;
+    }
+
+    /**
+     * Returns true if given AssessmentResults belong to the same assessment as this.
+     * Used to check for duplicates when adding an assessment to the address book.
+     * (unlikely to reach this as should check for duplicates in UniqueAssessmentList first before adding
+     * AssessmentResultsList).
+     */
+    public boolean isSameAssessmentResults(AssessmentResults ar) {
+        return this == ar
+                || (ar != null
+                && results.equals(ar.results));
+    }
+
+    /**
+     * Returns true if results list already contains the StudentResult.
+     */
+    public boolean contains(StudentResult toCheck) {
+        requireNonNull(toCheck);
+        return results.stream().anyMatch(toCheck::isSameStudentResult);
+    }
+
+    /**
+     * Adds a StudentResult to the list.
+     * The Student's result must not already be in the list.
+     * Can redefine this to allow updating for results already in the list.
+     */
+    public void add(StudentResult toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateStudentResultException();
+        }
+        results.add(toAdd);
+    }
+
+    public ObservableList<StudentResult> asUnmodifiableStudentResultsList() {
+        return unmodifiableResults;
+    }
+}
---
+++ src/main/java/seedu/address/model/assessment/exceptions/AssessmentNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.assessment.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified assessment.
+ */
+public class AssessmentNonFoundException extends RuntimeException{
+}
