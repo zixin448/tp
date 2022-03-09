dd30067d92268ec8af89c10bf42997784a63e881
Kevin <thamooie@yahoo.com.sg>
2022-03-10 02:16:29 +0800

    Remove print lines statements

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -44,7 +44,6 @@ public class LogicManager implements Logic {
         CommandResult commandResult;
         Command command = addressBookParser.parseCommand(commandText);
         commandResult = command.execute(model);
-        System.out.println(model.getFilteredTutorialList().size());
 
         try {
             storage.saveAddressBook(model.getAddressBook());
