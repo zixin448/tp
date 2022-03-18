8dbcec24d41c8e7f89e02775354fb594b8e28ec4
seanlaiys <68644109+seanlaiys@users.noreply.github.com>
2022-03-19 00:17:14 +0800

    Update src/main/java/seedu/address/logic/commands/ListClassCommand.java
    Co-authored-by: garethkoh <77238736+garethkoh@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/ListClassCommand.java
+++ src/main/java/seedu/address/logic/commands/ListClassCommand.java
@@ -16,7 +16,7 @@ public class ListClassCommand extends Command {
 
     public static final String MESSAGE_SUCCESS_ALL = "Listed all classes";
 
-    public static final String MESSAGE_SUCCESS = "Listed all classes on %1$s";
+    public static final String MESSAGE_SUCCESS_DAY = "Listed all classes on %1$s";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": List all classes or classes on a specified date "
             + "Parameters: " + PREFIX_DAY + "DAY\n"
