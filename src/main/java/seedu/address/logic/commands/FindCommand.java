172dc525e3ff306c958971e3ee814aca052a3d70
Yong Zi Xin <zixinyong448@gmail.com>
2022-03-25 13:07:32 +0800

    fix bug for display usual find

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -6,6 +6,11 @@ import seedu.address.commons.core.Messages;
 import seedu.address.model.DisplayType;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -29,9 +34,16 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        List<Person> all = new ArrayList<>();
         model.updateFilteredPersonList(predicate);
+        all.addAll(model.getFilteredPersonList());
+
+        // filter out duplicate contacts
+        List<Person> allNoDupes = new ArrayList<>(new LinkedHashSet<>(all));
+        model.setFilteredPersonsMultiPredList(allNoDupes);
+
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()), false,
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonsMultiPredList().size()), false,
                 false, DisplayType.FIND);
     }
 
