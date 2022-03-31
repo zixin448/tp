d7128ce18aae9d1d54926d70ee6f27769b4c9006
Yong Zi Xin <zixinyong448@gmail.com>
2022-03-31 21:47:59 +0800

    Add test for AddClassCommand

--- src/test/java/seedu/address/logic/commands/AddClassCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddClassCommandTest.java
@@ -1,2 +1,398 @@
-package seedu.address.logic.commands;public class AddClassCommandTest {
+package seedu.address.logic.commands;
+
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.assessment.Assessment;
+import seedu.address.model.assessment.AssessmentName;
+import seedu.address.model.assessment.Score;
+import seedu.address.model.assessment.StudentResult;
+import seedu.address.model.attendance.Attendance;
+import seedu.address.model.attendance.Comment;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.NusNetId;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Student;
+import seedu.address.model.tutorial.Tutorial;
+import seedu.address.model.tutorial.TutorialName;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTutorials.getTypicalAddressBook;
+
+public class AddClassCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void constructor_nullClass_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddClassCommand(null));
+    }
+
+    /**
+     * A default model stub that have all of the methods failing.
+     */
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasTutorial(Tutorial tutorial) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Tutorial getTutorialWithName(TutorialName tutorialName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addTutorial(Tutorial tutorial) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteTutorial(Tutorial tutorial) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setTutorial(Tutorial target, Tutorial editedTutorial) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public FilteredList<Person> getFilteredStudentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<StudentResult> getDisplayAssessmentResults() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateDisplayAssessmentResults(TutorialName tutName, AssessmentName assessmentName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Tutorial> getFilteredTutorialList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Assessment> getAssessmentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Assessment> getFilteredAssessmentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredAssessmentList(Predicate<Assessment> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasAssessment(Assessment assessment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addAssessment(Assessment toAdd) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasAssessmentWithName(AssessmentName name) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Assessment getAssessmentWithName(AssessmentName assessmentName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Assessment removeAssessmentWithName(AssessmentName name) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        public void updateFilteredTutorialList(Predicate<Tutorial> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredStudentList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public TutorialName getTutorialNameOfStudent(Name studentName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Attendance> getFilteredAttendanceList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addComment(Tutorial tutorial, NusNetId studentToComment, Comment toAdd) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void removeComment(Tutorial tutorial, NusNetId studentToRemoveComment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Comment getComment(Tutorial tutorial, NusNetId studentToViewComment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Comment> getCommentList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredAttendanceList(Tutorial tutorial, NusNetId studentId) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public FilteredList<Person> getAllStudentsList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addStudent(Student student) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void removeStudent(Student student) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasStudentWithName(Name studentName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasStudentResult(Name studentName, AssessmentName assessmentName) {
+            return false;
+        }
+
+        @Override
+        public void addStudentResult(Name studentName, AssessmentName assessmentName, Score sc) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void removeStudentResults(NusNetId studentId, TutorialName tutorialName) {
+            throw new AssertionError("This method should not be called.");
+        }
+        @Override
+        public void setStudentResult(Name studentName, AssessmentName assessmentName, Score sc) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasStudentWithId(NusNetId toAddStudentId) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void markAttendanceForClass(Tutorial tutorial, int week) {
+            throw new AssertionError("This method should not be called.");
+
+        }
+
+        @Override
+        public void markAttendanceForStudent(Tutorial tutorial, NusNetId studentId, int week) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void unmarkAttendanceForClass(Tutorial tutorial, int week) {
+            throw new AssertionError("This method should not be called.");
+
+        }
+
+        @Override
+        public void unmarkAttendanceForStudent(Tutorial tutorial, NusNetId studentId, int week) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasTutorialWithName(TutorialName tutorialName) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPersonWithName(Name name) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Person getPersonWithName(Name name) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonsMultiPredList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Student getStudentWithId(NusNetId id) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setFilteredPersonsMultiPredList(List<Person> persons) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean tutorialHasStudentWithId(NusNetId id, TutorialName tutorialName) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single tutorial class.
+     */
+    private class ModelStubWithClass extends ModelStub {
+        private final Tutorial tutorial;
+
+        ModelStubWithClass(Tutorial tutorial) {
+            requireNonNull(tutorial);
+            this.tutorial = tutorial;
+        }
+
+        @Override
+        public boolean hasTutorial(Tutorial tutorial) {
+            requireNonNull(tutorial);
+            return this.tutorial.isSameTutorial(tutorial);
+        }
+    }
+
+    /**
+     * A model stub that always accept the class being added.
+     */
+    private class ModelStubAcceptingClassAdded extends ModelStub {
+        final ArrayList<Tutorial> classesAdded = new ArrayList<>();
+        final FilteredList<Tutorial> filteredTutorials = new FilteredList<>((ObservableList<Tutorial>) classesAdded);
+
+        @Override
+        public boolean hasTutorial(Tutorial tutorial) {
+            requireNonNull(tutorial);
+            return classesAdded.stream().anyMatch(tutorial::isSameTutorial);
+        }
+
+        @Override
+        public void addTutorial(Tutorial tutorial) {
+            requireNonNull(tutorial);
+            classesAdded.add(tutorial);
+        }
+
+        @Override
+        public void updateFilteredTutorialList(Predicate<Tutorial> predicate) {
+            filteredTutorials.setPredicate(predicate);
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            return new AddressBook();
+        }
+    }
+
+
 }
