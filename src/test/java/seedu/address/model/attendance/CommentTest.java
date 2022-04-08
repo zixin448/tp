be91d90541e2c229571af2c3218e576095501fd3
Yong Zi Xin <zixinyong448@gmail.com>
2022-04-08 15:11:57 +0800

    update commentTest

--- src/test/java/seedu/address/model/attendance/CommentTest.java
+++ src/test/java/seedu/address/model/attendance/CommentTest.java
@@ -1,12 +1,14 @@
 package seedu.address.model.attendance;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.exceptions.CommandException;
 
 public class CommentTest {
+
+    Comment comment = new Comment("First comment");
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -25,5 +27,16 @@ public class CommentTest {
         // valid comment
         assertTrue(Comment.isValidComment("i")); // a single character
         assertTrue(Comment.isValidComment("Valid comment entered")); // a sentence
+    }
+
+    @Test
+    public void setCommentString() {
+
+        String newComment = "New comment";
+        Comment commentCopy = new Comment(comment.getCommentString());
+        commentCopy.setCommentString(newComment);
+
+        // update comment
+        assertEquals(commentCopy.getCommentString(), newComment);
     }
 }
