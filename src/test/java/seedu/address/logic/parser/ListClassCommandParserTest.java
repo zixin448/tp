ccf7a967e48194a200d3285c46af78d1cc9ca826
Yong Zi Xin <zixinyong448@gmail.com>
2022-04-09 11:50:00 +0800

    fix checkstyle error

--- src/test/java/seedu/address/logic/parser/ListClassCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListClassCommandParserTest.java
@@ -1,17 +1,20 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.ListClassCommand;
-import seedu.address.model.tutorial.Tutorial;
-import seedu.address.model.tutorial.TutorialHasSameDay;
-import seedu.address.testutil.TutorialBuilder;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.parser.ParserUtilTest.*;
+import static seedu.address.logic.parser.ParserUtilTest.TUTORIAL_DAY_DESC_1;
+import static seedu.address.logic.parser.ParserUtilTest.TUTORIAL_DAY_DESC_2;
+import static seedu.address.logic.parser.ParserUtilTest.TUTORIAL_NAME_DESC_1;
 import static seedu.address.testutil.TypicalTutorials.T01;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ListClassCommand;
+import seedu.address.model.tutorial.Tutorial;
+import seedu.address.model.tutorial.TutorialHasSameDay;
+import seedu.address.testutil.TutorialBuilder;
 
 public class ListClassCommandParserTest {
     private ListClassCommandParser parser = new ListClassCommandParser();
@@ -39,9 +42,15 @@ public class ListClassCommandParserTest {
     }
 
     @Test
-    public void parse_noField_success() {
+    public void parse_emptyField_success() {
         String emptyString = "";
         assertParseSuccess(parser, emptyString, new ListClassCommand(null));
+    }
+
+    @Test
+    public void parse_spaceField_success() {
+        String blankString = "  ";
+        assertParseSuccess(parser, blankString, new ListClassCommand(null));
     }
 
 }
