0069dc88731c2198b9466c24c106899c9f5823c4
Yong Zi Xin <zixinyong448@gmail.com>
2022-04-11 01:04:56 +0800

    add test TutorialNameIsEqualPredicateTest

---
+++ src/test/java/seedu/address/model/tutorial/TutorialNameIsEqualPredicateTest.java
@@ -0,0 +1,29 @@
+package seedu.address.model.tutorial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TutorialNameIsEqualPredicateTest {
+    @Test
+    public void equals() {
+        TutorialNameIsEqualPredicate predA = new TutorialNameIsEqualPredicate(new TutorialName("T01"));
+        TutorialNameIsEqualPredicate predB = new TutorialNameIsEqualPredicate(new TutorialName("T02"));
+
+        // same object -> returns true
+        assertTrue(predA.equals(predA));
+
+        // same value -> returns true
+        TutorialNameIsEqualPredicate predACopy = new TutorialNameIsEqualPredicate(new TutorialName("T01"));
+        assertTrue(predA.equals(predACopy));
+
+        // different type -> returns false
+        assertFalse(predA.equals(1));
+
+        // different value -> returns false
+        assertFalse(predA.equals(predB));
+
+        // null -> returns false
+        assertFalse(predA.equals(null));
+    }
+}
