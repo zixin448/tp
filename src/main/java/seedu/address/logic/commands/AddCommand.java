454f881bb32f18489a34e7fefce8e698aec7dc0a
garethkoh <e0533656@u.nus.edu>
2022-04-10 15:21:29 +0800

    Checkstyle error

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,8 +37,8 @@ public class AddCommand extends Command {
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
     public static final String MESSAGE_DUPLICATE_EMAIL = "This email already exists in the address book";
     public static final String MESSAGE_DUPLICATE_PHONE = "This phone number already exists in the address book";
-    public static final String MESSAGE_CANNOT_ADD_STUDENT_TAG = "The student tag is reserved for students. " +
-            "Student tag not added";
+    public static final String MESSAGE_CANNOT_ADD_STUDENT_TAG = "The student tag is reserved for students. "
+            + "Student tag not added";
 
     private final Person toAdd;
 
